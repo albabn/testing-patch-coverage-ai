bb307fb245a44cc64986b308e0f04a7226fdb5f7
Alba Bautista <alba.bautistanunez@datadoghq.com>
2025-08-25 14:20:13 +0200

    Eliminate lines to see if they find it executable

--- toy_calculator/test_calculator.py
+++ toy_calculator/test_calculator.py
@@ -10,21 +10,6 @@ def test_add():
     assert add(1.5, 2.5) == 4.0
 
 
-def test_subtract():
-    """Test the subtract function."""
-    assert subtract(5, 3) == 2
-    assert subtract(1, 1) == 0
-    assert subtract(0, 5) == -5
-    assert subtract(3.5, 1.5) == 2.0
-
-
-# def test_multiply():
-#     """Test the multiply function."""
-#     assert multiply(2, 3) == 6
-#     assert multiply(-2, 3) == -6
-#     assert multiply(0, 5) == 0
-#     assert multiply(2.5, 2) == 5.0
-
 
 def test_divide():
     """Test the divide function."""
